23d1e0dd0ca6ca49f09d8f9850a1e845390f07f4
Chaitanya Bhojane <chaitubhojane@gmail.com>
2024-02-16 01:08:51 +0530

    fix: username brief application

--- internal/domain/briefapplication.go
+++ internal/domain/briefapplication.go
@@ -15,18 +15,17 @@ const (
 )
 
 type BriefApplication struct {
-	Id       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserId   string             `bson:"userId,omitempty" json:"userId,omitempty"`
-	UserName string             `bson:"username,omitempty" json:"username,omitempty"`
-	Media    []string           `bson:"media,omitempty" json:"media,omitempty"`
-	Reel     []string           `bson:"reel,omitempty" json:"reel,omitempty"`
-	Note     string             `bson:"note,omitempty" json:"note,omitempty"`
-	BriefId  primitive.ObjectID `bson:"briefId,omitempty" json:"briefId,omitempty"`
-	Created  time.Time          `bson:"created,omitempty" json:"created,omitempty"`
-	Time     time.Time          `bson:"time,omitempty" json:"time,omitempty"`
-	Opened   bool               `bson:"opened" json:"opened"`
-	Status   int                `bson:"status,omitempty" json:"status,omitempty"`
-	User     *struct {
+	Id      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserId  string             `bson:"userId,omitempty" json:"userId,omitempty"`
+	Media   []string           `bson:"media,omitempty" json:"media,omitempty"`
+	Reel    []string           `bson:"reel,omitempty" json:"reel,omitempty"`
+	Note    string             `bson:"note,omitempty" json:"note,omitempty"`
+	BriefId primitive.ObjectID `bson:"briefId,omitempty" json:"briefId,omitempty"`
+	Created time.Time          `bson:"created,omitempty" json:"created,omitempty"`
+	Time    time.Time          `bson:"time,omitempty" json:"time,omitempty"`
+	Opened  bool               `bson:"opened" json:"opened"`
+	Status  int                `bson:"status,omitempty" json:"status,omitempty"`
+	User    *struct {
 		UserName string `bson:"username,omitempty" json:"username,omitempty"`
 	} `bson:"user,omitempty" json:"user,omitempty"`
 }
